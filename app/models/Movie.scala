f319bb6f8d0988e9ffafcc071fa4274b1eba8cb3
Carlos Daniel Sanchez <c@crowdswap.com>
2019-12-04 16:15:38 -0500

    Repository

--- app/models/Movie.scala
+++ app/models/Movie.scala
@@ -70,10 +70,32 @@ class MovieRepository @Inject()(
     db.run(createSchema)
   }
 
-  def getAll = ???
-  def getOne = ???
-  def create = ???
-  def update = ???
-  def delete = ???
+  def getAll = {
+    val q = movieQuery.sortBy(_.id)
+    db.run(q.result)
+  }
+  def getOne(id: String) = {
+    val q = movieQuery.filter(_.id === id)
+    db.run(q.result.headOption)
+  }
+  def create(movie: Movie) = {
+    val insert = movieQuery += movie
+    db.run(insert)
+      .flatMap(_ => getOne(movie.id.getOrElse("")))
+  }
+  def update(id: String, movie: Movie) = {
+    val q = movieQuery.filter(_.id === movie.id && movie.id.contains(id))
+    val update = q.update(movie)
+    db.run(update)
+      .flatMap(_ => db.run(q.result.headOption))
 
+  }
+  def delete(id: String) = {
+    val q = movieQuery.filter(_.id === id)
+
+    for {
+      objeto <- db.run(q.result.headOption)
+      _ <- db.run(q.delete)
+    } yield objeto
+  }
 }
